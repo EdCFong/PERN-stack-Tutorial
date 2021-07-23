60ac9bfa3855b521f9997b41a791dc728f645fd8
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2021-07-22 22:46:37 -0400

    Update July 22, 2021

--- server/server.js
+++ server/server.js
@@ -1,13 +1,36 @@
 require("dotenv").config();
 const express = require("express");
-
+const morgan = require("morgan");
 const app = express();
 
+app.use((req, res, next) =>{
+ next();
+});
+
+
+//Get all Restaurants
 app.get("/getRestaurants", (req, res) => {
-  res.json({
-      status: "success"
+  res.status(200).json({
+      status: "success",
+      data: {
+restaurant:["mcdonals", "wendys"]
+      }
   })
 });
+
+//Get a restaurant 
+app.get("/api/v1/restaurants/:id", (req, res) => {
+  console.log(req.params)
+})
+
+//Create a restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+  console.log(req)
+})
+
+
+
+
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
